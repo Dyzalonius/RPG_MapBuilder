a9578647692231e07dbeb4b31b50d30eb96454f7
dyzalonius <dyzalonius@gmail.com>
2019-06-18 15:46:33 +0200

    Initial project.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMotor playerMotor;
+
+    private void Start()
+    {
+        playerMotor = GetComponent<PlayerMotor>();
+    }
+	
+	private void Update()
+    {
+        // Get movement input
+        Vector3 velHorizontal = transform.right * Input.GetAxis("Horizontal");
+        Vector3 velApplicate = transform.forward * Input.GetAxis("Applicate");
+        Vector3 velVertical = transform.up * Input.GetAxis("Vertical");
+        Vector3 velocity = (velHorizontal + velApplicate + velVertical);
+        playerMotor.velocity = velocity;
+
+         // Get rotation input
+         Vector3 rotation = Vector3.zero;
+        if (Input.GetMouseButton(1))
+        {
+            rotation.y = Input.GetAxisRaw("Mouse X");
+            rotation.x = Input.GetAxisRaw("Mouse Y");
+        }
+        playerMotor.rotation = rotation;
+
+        // Get movementspeedfactor input
+        float movementSpeedFactor = 1;
+        if (Input.GetKey(KeyCode.LeftShift))
+            movementSpeedFactor *= 10;
+        playerMotor.movementSpeedFactor = movementSpeedFactor;
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    [HideInInspector] public Vector3 velocity;
+    [HideInInspector] public Vector3 rotation;
+    [HideInInspector] public float movementSpeedFactor;
+
+    [Header("Settings")]
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeedHorizontal;
+    [SerializeField] private float rotationSpeedVertical;
+    [SerializeField] private float cameraRotationLimit;
+
+    private Rigidbody rigidBody;
+    private Camera cam;
+    private float camCurrentRotationX;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        // Move
+        if (velocity != Vector3.zero)
+            rigidBody.MovePosition(rigidBody.position + velocity * movementSpeed * movementSpeedFactor * Time.fixedDeltaTime);
+    }
+
+    private void Rotate()
+    {
+        // Rotate Y, separate from X so UP stays UP.
+        rigidBody.MoveRotation(rigidBody.rotation * Quaternion.Euler(0, rotation.y * rotationSpeedHorizontal, 0));
+
+        // Rotate X
+        if (cam != null)
+        {
+            camCurrentRotationX -= rotation.x * rotationSpeedVertical;
+            camCurrentRotationX = Mathf.Clamp(camCurrentRotationX, -cameraRotationLimit, cameraRotationLimit);
+            cam.transform.localEulerAngles = new Vector3(camCurrentRotationX, 0f, 0f);
+        }
+    }
+}
